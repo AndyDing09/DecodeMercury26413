5d8db6e2efabf768003c7f15dbebcb3e083e4480
GNCE Mercury 26413 <28parksz@weston.org>
2026-02-24 17:52:03 -0500

    start for angle adjustment and shootig whike moving

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ShooterConstants.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.robot.constants;
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.math.MathFunctions;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+@Configurable
+public class ShooterConstants {
+
+    public static Pose GOAL_POS_RED = new Pose(138, 138);
+    public static Pose GOAL_POS_BLUE = GOAL_POS_RED.mirror();
+
+    public static double SCORE_HEIGHT = 26; // inches
+    public static double SCORE_ANGLE = Math.toRadians(-30); // radians
+    public static double PASS_THROUGH_POINT_RADIUS = 5; // inches
+}
