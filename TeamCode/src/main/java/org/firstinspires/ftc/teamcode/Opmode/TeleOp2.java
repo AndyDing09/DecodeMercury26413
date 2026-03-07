cc732808327ddcbeef90dd12996e0a7ca4954e3d
ArnieFTC <arnavsemail@gmail.com>
2026-03-06 22:42:13 -0500

    diwhiuehuiqwdh tele

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Opmode/TeleOp2.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.VoltageSensor;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Shooter;
-import org.firstinspires.ftc.teamcode.subsystems.Turret;
+// import org.firstinspires.ftc.teamcode.subsystems.Turret;
 
 @TeleOp(name = "TeleOp with Turret Tracking - Fixed", group = "TeleOp")
 public class TeleOp2 extends LinearOpMode {
@@ -15,7 +15,7 @@ public class TeleOp2 extends LinearOpMode {
     private Drivetrain drivetrain;
     private Intake intake;
     private Shooter shooter;
-    private Turret turret;
+    // private Turret turret;
     private VoltageSensor voltageSensor;
 
     @Override
@@ -23,20 +23,20 @@ public class TeleOp2 extends LinearOpMode {
         drivetrain    = new Drivetrain(hardwareMap);
         intake        = new Intake(hardwareMap);
         shooter       = new Shooter(hardwareMap);
-        turret        = new Turret(hardwareMap);
+      //   turret        = new Turret(hardwareMap);
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
         telemetry.addLine("Ready. GP1: Y/X=Hood Up/Down | GP2: UP/DOWN=cycle param, LEFT/RIGHT=adjust, LB=coarse, A=reset enc, X=track");
         telemetry.update();
 
-        turret.centerTurret(this);
+       //  turret.centerTurret(this);
 
         waitForStart();
 
         shooter.initControllers();
 
         while (opModeIsActive()) {
-            if (gamepad2.a) turret.resetEncoder(this);
+        //     if (gamepad2.a) turret.resetEncoder(this);
 
             // Drive
             drivetrain.drive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
@@ -53,16 +53,16 @@ public class TeleOp2 extends LinearOpMode {
             shooter.updatePIDF(voltageSensor, telemetry);
 
             // Turret
-            turret.updateTuning(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.dpad_left, gamepad2.dpad_right, gamepad2.left_bumper);
-            turret.update(gamepad2.cross, gamepad2.right_stick_x, voltageSensor, telemetry, this);
+         //    turret.updateTuning(gamepad2.dpad_up, gamepad2.dpad_down, gamepad2.dpad_left, gamepad2.dpad_right, gamepad2.left_bumper);
+         //    turret.update(gamepad2.cross, gamepad2.right_stick_x, voltageSensor, telemetry, this);
 
             // Telemetry
-            turret.addTelemetry(telemetry);
+      //       turret.addTelemetry(telemetry);
             telemetry.addData("Shooter RPM", (int) shooter.getTargetRPM());
             telemetry.addData("Hood Pos", String.format("%.3f", shooter.getHoodAnglePos()));
             telemetry.update();
         }
 
-        turret.stop();
+     //    turret.stop();
     }
 }
